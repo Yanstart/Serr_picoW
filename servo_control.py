a0dfc05a96c03b74a2040acbfdc20ef84cc49bdf
noel junior yando <67079434+Yanstart@users.noreply.github.com>
2024-04-24 16:02:08 +0200

    Update servo_control.py
    version fonctionnelle

--- servo_control.py
+++ servo_control.py
@@ -1,26 +1,19 @@
+from machine import Pin, Timer, ADC, PWM
+import utime
 import time
-import RPi.GPIO as GPIO
 
-class ServoControl:
-    def __init__(self, pin):
-        self.pin = pin
-        GPIO.setup(self.pin, GPIO.OUT)
-        self.servo = GPIO.PWM(self.pin, 50)  # Fréquence de 50 Hz pour le servo
-        self.servo.start(0)
+servoPin = Pin(16)
+servo = PWM(servoPin)
+duty_cycle = 0
+servo.freq(50)  
+def setservo(angle):
+    duty_cycle = int(angle*(7803-1950)/180) + 1950
+    servo.duty_u16(duty_cycle)
+setservo(90)
+while 1:
+    setservo(0)
+    time.sleep(1)
+    setservo(180)
+    time.sleep(1)
+    setservo(0)
 
-    def set_angle(self, angle):
-        duty = angle / 18 + 2
-        GPIO.output(self.pin, True)
-        self.servo.ChangeDutyCycle(duty)
-        time.sleep(1)
-        GPIO.output(self.pin, False)
-        self.servo.ChangeDutyCycle(0)
-
-    def control_servo(self, temperature):
-        seuil_temperature = 25
-        if temperature > seuil_temperature:
-            # Ouvrir la porte
-            self.set_angle(90)
-        else:
-            # Fermer la porte
-            self.set_angle(0)
